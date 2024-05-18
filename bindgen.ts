b72c4c9af3db7112002662839d8ff45a5265fd42
thegu5 <58223632+thegu5@users.noreply.github.com>
2024-05-18 14:35:10 -0400

    ts class shells, generics, inheritance

--- bindgen.ts
+++ bindgen.ts
@@ -4,14 +4,41 @@ import * as recast from "recast";
 
 const typedata = JSON.parse(fs.readFileSync(process.argv[process.argv.length - 1]).toString()) as {
     Key: string,
-    Value: { Name: string, BaseType: string | null, Namespace: string, IsStruct: boolean, InheritanceDepth: number, Fields: any[], Methods: any[] }
+    Value: {
+        Name: string,
+        BaseType: string | null,
+        Namespace: string,
+        IsStruct: boolean,
+        InheritanceDepth: number,
+        Fields: any[],
+        Methods: any[]
+    }
 }[];
 
 const ast = recast.parse("", {
     parser: require("recast/parsers/typescript")
 });
+function formatPropName(name: string) {
+    return name.replace(/[=/]/g, "_");
+}
+
 const b = recast.types.builders;
 typedata.forEach((t) => {
-    ast.program.body.push(b.tsModuleDeclaration(b.identifier(t.Value.Namespace == "" ? "Il2Cpp" : "Il2Cpp." + t.Value.Namespace), b.tsModuleBlock([])));
+    if (t.Value.Name[0] == '<' || t.Value.Name[0] == '>') return; // weird anonymous type, nested class stuff (TODO!)
+    ast.program.body.push(
+        b.exportNamedDeclaration(
+            b.tsModuleDeclaration(
+                b.identifier(t.Value.Namespace == "" ? "Il2Cpp" : "Il2Cpp." + t.Value.Namespace), 
+                b.tsModuleBlock([
+                    b.exportNamedDeclaration(
+                        b.classDeclaration(
+                            b.identifier(formatPropName(t.Value.Name)),
+                            b.classBody(
+                                []
+                            ),
+                            t.Value.BaseType ? b.identifier(formatPropName(t.Value.BaseType)) : null
+                        )
+                    )
+                ]))));
 })
 console.log(recast.print(ast).code);
