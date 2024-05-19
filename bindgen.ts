b4f30a54139ddc97e3e2c8a94b8872c227dc5eea
thegu5 <58223632+thegu5@users.noreply.github.com>
2024-05-19 14:00:22 -0400

    create types for json data, refactor class ast node creation to function

--- bindgen.ts
+++ bindgen.ts
@@ -2,17 +2,34 @@ import * as process from "node:process";
 import * as fs from "node:fs";
 import * as recast from "recast";
 
+interface Il2CppClass {
+    Name: string,
+    BaseType: string | null,
+    Namespace: string,
+    IsStruct: boolean,
+    InheritanceDepth: number,
+    Fields: Il2CppField[],
+    Methods: Il2CppMethod[],
+    NestedClasses: Il2CppClass[]
+}
+interface Il2CppField {
+    Name: string,
+    Offset: number,
+    Type: string
+}
+interface Il2CppMethod {
+    Name: string,
+    Parameters: { Name: string, Type: string }[],
+    ReturnType: string | null,
+    IsStatic: boolean,
+    Index: number | null,
+    MethodInfoPtr: number
+}
+
+
 const typedata = JSON.parse(fs.readFileSync(process.argv[process.argv.length - 1]).toString()) as {
     Key: string,
-    Value: {
-        Name: string,
-        BaseType: string | null,
-        Namespace: string,
-        IsStruct: boolean,
-        InheritanceDepth: number,
-        Fields: any[],
-        Methods: any[]
-    }
+    Value: Il2CppClass
 }[];
 
 const ast = recast.parse("", {
@@ -23,21 +40,25 @@ function formatPropName(name: string) {
 }
 
 const b = recast.types.builders;
-typedata.forEach((t) => {
-    if (t.Value.Name[0] == '<' || t.Value.Name[0] == '>') return; // weird anonymous type, nested class stuff (TODO!)
+
+function classToSyntax(data: Il2CppClass) : recast.types.namedTypes.ClassDeclaration {
+    return b.classDeclaration(
+        b.identifier(formatPropName(data.Name)),
+        b.classBody([]),
+        data.BaseType ? b.identifier("Il2Cpp." + formatPropName(data.BaseType)) : null
+    )
+}
+
+typedata.forEach((arrelem) => {
+    let t = arrelem.Value;
+    if (t.Name[0] == '<' || t.Name[0] == '>') return; // weird anonymous type, nested class stuff (TODO!)
     ast.program.body.push(
         b.exportNamedDeclaration(
             b.tsModuleDeclaration(
-                b.identifier(t.Value.Namespace == "" ? "Il2Cpp" : "Il2Cpp." + t.Value.Namespace), 
+                b.identifier(t.Namespace == "" ? "Il2Cpp" : "Il2Cpp." + t.Namespace), 
                 b.tsModuleBlock([
                     b.exportNamedDeclaration(
-                        b.classDeclaration(
-                            b.identifier(formatPropName(t.Value.Name)),
-                            b.classBody(
-                                []
-                            ),
-                            t.Value.BaseType ? b.identifier("Il2Cpp." + formatPropName(t.Value.BaseType)) : null
-                        )
+                        classToSyntax(t)
                     )
                 ]))));
 })
