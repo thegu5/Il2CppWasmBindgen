e12f2167c89421f6ff0c91ae656a64fc1d1d31e7
thegu5 <58223632+thegu5@users.noreply.github.com>
2024-06-08 22:10:37 -0400

    hacky fix for interface-implementing property type load errors

--- InterfaceMethodFixProcessingLayer.cs
+++ InterfaceMethodFixProcessingLayer.cs
@@ -4,18 +4,22 @@ using Cpp2IL.Core.Model.Contexts;
 
 namespace Il2CppTsBindgen;
 
-// TODO: determine root cause (after fixing assembly resolve events)
+// TODO: determine root cause
 public class InterfaceMethodFixProcessingLayer : Cpp2IlProcessingLayer
 {
     public override void Process(ApplicationAnalysisContext appContext, Action<int, int>? progressCallback = null)
     {
         foreach (var type in appContext.AllTypes)
         {
-            if (type.Name.Contains("FarWaypointDelay")) Debugger.Break();
-            foreach (var prop in type.Properties.Where(method => method.Name.Contains('.') && !method.Name.StartsWith('.') && !method.Name.StartsWith('<')))
+            foreach (var property in type.Properties.Where(method => method.Name.Contains('.') && !method.Name.StartsWith('.') && !method.Name.StartsWith('<')))
             {
-                Console.WriteLine("Found violating property: " + prop.Name);
-                prop.OverrideName = prop.Name.Split('.').Last();
+                property.OverrideName = property.Name.Split('.').Last();
+                Console.WriteLine($"Changing {type.Name}'s {property.DefaultName} to {property.OverrideName}");
+            }
+            foreach (var method in type.Methods.Where(method => method.Name.Contains('.') && !method.Name.StartsWith('.') && !method.Name.StartsWith('<')))
+            {
+                method.OverrideName = method.Name.Split('.').Last();
+                Console.WriteLine($"Changing {type.Name}'s {method.DefaultName} to {method.OverrideName}");
             }
         }
     }
